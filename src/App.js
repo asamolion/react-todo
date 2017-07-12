758d26e8129f0c0cbd811cab68d462149220ef00
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2017-07-12 16:55:52 +0500

    input and list handling complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class Task extends Component {}
 
 class App extends Component {
+  
+
+  constructor() {
+    super();
+    this.getTaskList = this.getTaskList.bind(this);
+    this.setTaskList = this.setTaskList.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.addTask = this.addTask.bind(this);
+    this.state = {
+      tasks: this.getTaskList(),
+      inputValue: ""
+    };
+  }
+
+  getTaskList() {
+    var tasks = JSON.parse(localStorage.getItem("tasks"));
+    return tasks ? tasks : [];
+  }
+
+  setTaskList(task) {
+    if (!task) {
+      return;
+    }
+    this.state.tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(this.state.tasks));
+  }
+
+  handleChange(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+
+  addTask() {
+    this.setTaskList(this.state.inputValue);
+    this.setState({
+      tasks: this.getTaskList()
+    });
+  }
+
   render() {
+    const taskList = this.state.tasks.map((task) => 
+      <li>{ task }</li>
+    );
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>The 10001st Todo App</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="inputField">
+          <input type="text" onChange={this.handleChange} />
+          <button onClick={this.addTask}>Add Task</button>
         </p>
+        <ul>
+          {taskList}
+        </ul>
       </div>
     );
   }
